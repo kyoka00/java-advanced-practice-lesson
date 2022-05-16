7428034dd8260f7093351819034c2146390421c2
Maeno <22maeno_kyoka@axiz.co.jp>
2022-05-16 17:49:16 +0900

    課題発展途中

--- src/main/java/Animal/Cat.java
+++ src/main/java/Animal/Cat.java
@@ -1,18 +1,42 @@
 package Animal;
 
 public class Cat extends Animal{
-	public Cat(String name, int age){
+	public Cat(){
 		super();
-		this.name = name;
-		this.age = age;
+
 	}
 	@Override
 	public String introduceAge() {
+		String humanAge;
+		switch(age) {
+		case 0:
+			humanAge = "0～10代中盤";
+			break;
+		case 1:
+			humanAge = "10代中盤";
+			break;
+		case 2, 3:
+			humanAge = "20代";
+			break;
+		case 4,5:
+			humanAge = "30代";
+			break;
+		case 6,7,8:
+			humanAge = "40代";
+			break;
+		case 9, 10:
+			humanAge = "50代";
+			break;
+		default:
+			humanAge = "60歳以上";
+			break;
+		}
+		String msg ="種類はネコです。人間で言うと、"+ humanAge + "です。";
 		return "";
 	}
 	
 	@Override
 	public String getFilePath() {
-		return "C:";
+		return "C:\\work/cat.txt";
 	}
 }
--- src/main/java/Animal/Rabbit.java
+++ src/main/java/Animal/Rabbit.java
@@ -1,18 +1,42 @@
 package Animal;
 
 public class Rabbit extends Animal{
-	public Rabbit(String name,int age){
+	public Rabbit(){
 		super();
-		this.name = name;
-		this.age = age;
+		
 	}
 	@Override
 	public String introduceAge() {
-		return "";
+		String humanAge;
+		switch (age) {
+		case 0:
+			humanAge = "0~10代中盤";
+			break;
+		case 1:
+			humanAge = "10代中盤";
+			break;
+		case 2:
+			humanAge = "20代";
+		 	break;
+		case 3:
+			humanAge = "30代";
+		 	break;
+		case 4:
+			humanAge = "40代";
+		 	break;
+		case 5:
+			humanAge = "50代";
+		 	break;
+		default:
+			humanAge = "60歳以上";
+			break;
+		}
+		String msg ="種類はウサギです。人間でいうと"+ humanAge + "です";
+		return msg;
 	}
 	
 	@Override
 	public String getFilePath() {
-		return "C:";
+		return "C:\\work/rabbit.txt";
 	}
 }
